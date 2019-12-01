94bb6e2171a43197f027ed137da8fdd209e54e68
Pierre-marie LAM <plam@e2r8p8.42.fr>
2019-12-01 12:16:26 +0100

    itoa_ult corrected & add some new functions

--- printfunctions.c
+++ printfunctions.c
@@ -6,7 +6,7 @@
 /*   By: plam <plam@student.42.fr>                  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/11/19 13:23:47 by plam              #+#    #+#             */
-/*   Updated: 2019/12/01 12:12:42 by plam             ###   ########.fr       */
+/*   Updated: 2019/12/01 12:15:47 by plam             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -29,16 +29,18 @@ void	hexa(char *offset, int hex)
 
 	i = 0;
 	if (offset[i] == '%')
-		if (offset[i + 1] == 'p')
-		{
-			disp = ft_itoa_ult(hex, "0123456789abcdef");
-			write(1, "0x", 2);
-			write(1, disp, ft_strlen(disp));
-			write(1, "\n", 1);
-		}
+		if (offset[i + 1] == '%')
+			write (1, "%", 1);
 		else if (offset[i + 1] == 'd' || offset[i + 1] == 'i')
 		{
 			disp = ft_itoa_ult(hex, "0123456789");
+			write(1, disp, ft_strlen(disp));
+			write(1, "\n", 1);
+		}
+		else if (offset[i + 1] == 'p')
+		{
+			disp = ft_itoa_ult(hex, "0123456789abcdef");
+			write(1, "0x", 2);
 			write(1, disp, ft_strlen(disp));
 			write(1, "\n", 1);
 		}
