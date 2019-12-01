6138f50daba0a5ab7655de8ca1eae2417430a14d
Pierre-marie LAM <plam@e2r8p8.42.fr>
2019-12-01 11:56:33 +0100

    itoa_ult corrected & add some new functions

--- main.c
+++ main.c
@@ -6,7 +6,7 @@
 /*   By: plam <plam@student.42.fr>                  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/11/19 11:20:20 by plam              #+#    #+#             */
-/*   Updated: 2019/12/01 11:35:44 by plam             ###   ########.fr       */
+/*   Updated: 2019/12/01 11:56:00 by plam             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,7 +20,7 @@ int	main(int c, char **v)
 		printf("%p\n", 256);
 		printf("%s\n", ft_itoa_ult(42, "0123456789ABCDEF"));
 		hexa(v[1], atoi(v[2]));
-		printf("%p\n", atoi(v[2]));
+		printf("%d\n", atoi(v[2]));
 	}
 	return (0);
 }
