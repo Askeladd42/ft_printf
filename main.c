b4d9f33a9bf135858a7be9a22d043144bab283a9
Pierre-marie LAM <plam@e3r9p6.42.fr>
2020-02-18 19:07:26 +0100

    check for errors and correction

--- main.c
+++ main.c
@@ -6,7 +6,7 @@
 /*   By: plam <plam@student.42.fr>                  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/11/19 11:20:20 by plam              #+#    #+#             */
-/*   Updated: 2020/02/18 18:54:42 by plam             ###   ########.fr       */
+/*   Updated: 2020/02/18 19:07:14 by plam             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,15 +15,15 @@
 
 int	main(void)
 {
-	//int c = 45;
-	//char *s = NULL;
-	//unsigned int i = 12589;
-	/*printf("OG : c = %p\n\n", &c);
+	int c = 45;
+	char *s = NULL;
+	unsigned int i = 12589;
+	printf("OG : c = %p\n\n", &c);
 	ft_printf("ft : c = %p\n\n", &c);
 	printf("%i\n", printf("%p\n", &s));
 	ft_printf("%i\n", ft_printf("%p\n", &s));
 	printf("%i\n", printf("%u\n", i));
-	ft_printf("%i\n", ft_printf("%u\n", i));*/
+	ft_printf("%i\n", ft_printf("%u\n", i));
 
 	/*printf("|%*.*i|\n", -1, -12, 758);
 	ft_printf("ft_printf = %i\n", ft_printf("|^.^/%*.*s^.^/|\n", -85, -8, s));
@@ -32,31 +32,29 @@ int	main(void)
 	printf("%p\n", &s);
 	ft_printf("|%10.6x|\n", 46654);*/
 
-	/*ft_printf("%p", &c);*/
-	/*ft_printf("%i", ft_printf("\\!/%.p\\!/", s));
-	printf("\n");
-	printf("%i", printf("\\!/%.p\\!/", s));*/
-	//printf("\n\n");
-	/*printf("%-7.1s", s);
-	ft_printf("%-7.1s", s);*/
+	/*ft_printf("%p\n", &c);
+	ft_printf("%i", ft_printf("\\!/%.p\\!/\n", s));
+	printf("%i", printf("\\!/%.p\\!/\n", s));
+	printf("\n\n");
+	printf("%-7.1s\n", s);
+	ft_printf("%-7.1s\n", s);*/
 	
-	/*ft_printf("%i", ft_printf("\\!/42%19.30s42\\!/", "4560635760"));
-	printf("\n");
-	printf("%i", printf("\\!/42%19.30s42\\!/", "4560635760"));*/
+	/*ft_printf("%i", ft_printf("\\!/42%19.30s42\\!/\n", "4560635760"));
+	printf("%i", printf("\\!/42%19.30s42\\!/\n", "4560635760"));*/
 	
-	printf("i = %d\n",printf("%%d%%%*s%%", 5, "hi"));
-	ft_printf("i = %d\n",ft_printf("%%d%%%*s%%", 5, "hi"));
+	/*printf("i = %d\n",printf("%%d%%%*s%%", 5, "hi"));
+	ft_printf("i = %d\n",ft_printf("%%d%%%*s%%\n", 5, "hi"));*/
 
 	/*ft_printf("%%%d%c%d%i%u%x%%%X", 45, 'd', 242, 54545, -53424345, i, i);
 	printf("\n\n\n");
-	printf("%%%d%c%d%i%u%x%%%X", 45,'d', 242, 54545, -53424345, i, i);*/
-	/*printf("%%%d%p%s%c%d%i%u%x%%%X", 45, &c, "HELLOOOOLJLJL<<", 'd', 242, 54545, -53424345, i, i);
+	printf("%%%d%c%d%i%u%x%%%X", 45,'d', 242, 54545, -53424345, i, i);
+	printf("%%%d%p%s%c%d%i%u%x%%%X", 45, &c, "HELLOOOOLJLJL<<", 'd', 242, 54545, -53424345, i, i);
 	printf("\n\n\n");
 	ft_printf("%%%d%p%s%c%d%i%u%x%%%X", 45, &c, "HELLOOOOLJLJL<<", 'd', 242, 54545, -53424345, i, i);*/
 	
 	
-	//ft_printf("go of %3.2s\n", NULL);
-	/*int p = printf("OG Printf = %8.3i\n", -8473);
+	/*ft_printf("go of %3.2s\n", NULL);
+	int p = printf("OG Printf = %8.3i\n", -8473);
 	int fp = ft_printf("ft_printf = %8.3i\n", -8473);
 	printf("OG Printf count = %i\nft_printf count = %i\n", p, fp);*/
 	return (0);
