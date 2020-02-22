1c2cf8d7052961e2f4bcafce0f01f619a05d703c
Pierre-marie LAM <plam@e3r7p11.42.fr>
2020-02-22 14:21:01 +0100

    check for errors and correction

--- srcs/ft_strlen.c
+++ srcs/ft_strlen.c
@@ -6,7 +6,7 @@
 /*   By: plam <plam@student.42.fr>                  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/10/07 13:43:09 by plam              #+#    #+#             */
-/*   Updated: 2020/02/22 13:35:27 by plam             ###   ########.fr       */
+/*   Updated: 2020/02/22 14:20:51 by plam             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -34,7 +34,6 @@ void	condition_zero(t_print *printer, size_t z, size_t sp)
 			printer->buff[0] = '0';
 		z--;
 	}
-	printf("size = %d, sp = %zu, z = %zu, buff = %s\n", printer->size, sp, z, printer->buff);
 	if (!(printer->flags & POINT))
 	{
 		if (printer->buff[0] == '0' && ft_atoi_simple(printer->buff) != 0)
