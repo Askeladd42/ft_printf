a17efc64f76f2415abb3d6cfb91ac0060f10ebec
Pierre-marie LAM <plam@e3r8p8.42.fr>
2020-02-22 15:49:23 +0100

    check for errors and correction

--- srcs/ft_strlen.c
+++ srcs/ft_strlen.c
@@ -6,12 +6,11 @@
 /*   By: plam <plam@student.42.fr>                  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/10/07 13:43:09 by plam              #+#    #+#             */
-/*   Updated: 2020/02/22 15:46:05 by plam             ###   ########.fr       */
+/*   Updated: 2020/02/22 15:49:16 by plam             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../includes/libftprintf.h"
-#include <stdio.h>
 
 size_t	ft_strlen(char *str)
 {
@@ -29,8 +28,9 @@ void	check_neg_zero(t_print *printer, size_t z)
 {
 	if (printer->buff[0] == '-')
 	{
-		if ((!(printer->flags & POINT) && printer->size > (int)printer->index) ||
-		(printer->acc > (int)printer->index && (ft_atoi_simple(printer->buff) != 0)))
+		if ((!(printer->flags & POINT) && printer->size > (int)printer->index)
+		|| (printer->acc > (int)printer->index
+			&& (ft_atoi_simple(printer->buff) != 0)))
 			printer->buff[0] = '0';
 		z--;
 	}
